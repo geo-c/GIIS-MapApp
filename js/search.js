17a8d4b5360460f4e470fcbdc1422a2eea08f412
Saad Sarfraz <thedawn786@gmail.com>
2017-02-06 21:50:43 +0100

    update user hint list when particular specie type is selected

--- js/search.js
+++ js/search.js
@@ -5,6 +5,8 @@ var listOfParks;
 
 var filteredParks;
 
+prepareDefaultDataList();
+
 /**
  * @desc send request to server and retrieve parks having requested specie
  */
@@ -12,7 +14,7 @@ $('#search').click(function(){
     var text = $('#search_text').val();
     
 
-    
+    //query to retrieve list of parks where a specie is found
     query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://www.heppnetz.de/ontologies/goodrelations/v1#>\nSelect  ?parkname \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?a txn:scientificName '+'"'+text+'"'+'.\n?a wd:livesIn ?b.\n?b foaf:name ?parkname\n}\n}';
 
 
@@ -22,7 +24,7 @@ $('#search').click(function(){
     $.ajax({
     url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query),
     method: "GET",
-        async:false,
+    async:false,
     // dataType: "jsonp",
     success: function(result){
         
@@ -39,10 +41,55 @@ $('#search').click(function(){
     });
 });
 
+//invoked when user select any of search criteria //Mammals, Birds etc.
+$( "select.selectpicker" ).change(function() {
+
+    $( "select option:selected" ).each(function() {
+        let specie =  $( this ).val();
+
+        if(specie == 'all'){
+            //reset inner list
+            $('#specieslist').text(' ');
+            $('#search_text').val('');
+            prepareDefaultDataList();
+        }
+        //alert("hi" +specie );
+        if(specie == 'Bird' || specie == 'Mammal'||
+            specie == 'Reptile'||specie == 'Anfibian'){ //misspelling on server
+                //get all species
+                let query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://www.heppnetz.de/ontologies/goodrelations/v1#>\nSelect DISTINCT  ?specielist  \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?a txn:scientificName ?specielist. \n?a bio:class '+'"'+specie+'"'+'.\n}\n}';
+                //alert(query);
+                $.ajax({
+                url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query),
+                method: "GET",
+                async:false,
+                // dataType: "jsonp",
+                success: function(result){
+                    
+                var specielist = result.results.bindings;
+                
+                //reset inner list
+                $('#specieslist').text(' ');
+                $('#search_text').val('');
+                prepareDataList(specielist);
+                    
+
+
+                }, 
+                error: function(xhr, textStatus, errorThrown){ 
+                    alert("Unable to fetch Server data");             	 	
+                }
+                });
+        }
+    });
+
+    
+});
+
 /**
  * @desc inserts parks found to side frame
  *@param list of parks retrieved from server
-    */
+*/
 
 function insertParks(listOfParks) {
     var sidepanel = document.getElementById('sidepanel');
@@ -67,32 +114,34 @@ function insertParks(listOfParks) {
     }
 }
 
-//prepare datalist to provide hints to user
+/**
+ * @desc prepare datalist to provide hints to user
+ *       will retrieve all species from server
+ */
+function prepareDefaultDataList(){
 
-var query2 = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://www.heppnetz.de/ontologies/goodrelations/v1#>\nSelect DISTINCT  ?specielist  \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?a txn:scientificName ?specielist \n}\n}';
+    var query2 = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://www.heppnetz.de/ontologies/goodrelations/v1#>\nSelect DISTINCT  ?specielist  \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?a txn:scientificName ?specielist \n}\n}';
+
+    $.ajax({
+        url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query2),
+        method: "GET",
+            async:false,
+        // dataType: "jsonp",
+        success: function(result){
+            
+            var specielist = result.results.bindings;
+            // alert(specielist[0].specielist.value);
+            prepareDataList(specielist);
+            
 
 
-// alert(query);
-
-
-$.ajax({
-url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query2),
-method: "GET",
-    async:false,
-// dataType: "jsonp",
-success: function(result){
-    
-    var specielist = result.results.bindings;
-    // alert(specielist[0].specielist.value);
-    prepareDataList(specielist);
-    
-
-
-}, 
-error: function(xhr, textStatus, errorThrown){ 
-    alert("Unable to fetch Server data");             	 	
+        }, 
+        error: function(xhr, textStatus, errorThrown){ 
+            alert("Unable to fetch Server data");             	 	
+        }
+        });
 }
-});
+
 
 function prepareDataList(specielist){
     for(i in specielist){
