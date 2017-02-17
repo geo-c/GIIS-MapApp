f144078505f4cff2145078e5dc38c696b8187871
Saad Sarfraz <thedawn786@gmail.com>
2017-02-17 16:54:48 +0100

    added documentation

---
+++ GIIS-Application/js/chart.js
@@ -0,0 +1,303 @@
+ $(document).ready(function() {
+    getBiodiversityChart();
+});
+	var inter;
+	var user_input;
+	var user_option;
+	function getBiodiversityChart() {
+		 document.getElementsByClassName('low')[0].style.backgroundColor = "#99ff99";
+		 document.getElementsByClassName('medium')[0].style.backgroundColor = "#339933";
+		 document.getElementsByClassName('high')[0].style.backgroundColor = "#003300";
+		 user_input=$("#Index").val();
+		 user_input=parseFloat(user_input);
+		 user_option=$("#DI").val();
+	var query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://purl.org/goodrelations/v1#>\nSelect ?p ?value ?n ?count ?area \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?p prop:hasProperty ?parks.\n?p foaf:name ?n.\n?parks uco:propertyName "BiodiversityIndex".\n?parks gr:hasValue ?value.\n?p pr:SIO_000955.rdf ?sp.\n?sp pr:SIO_000794.rdf ?count.\n?p mea:Area ?b_area.\n?b_area gr:hasValue ?area.}\n}order By ?n';
+
+    var dataPoints = [];
+	var park_area=[];
+	$.ajax({
+        url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query),
+        method: "GET",
+        dataType: "jsonp",
+        async:false,
+        success: function(result){
+		//console.log(result);
+		for (var i in result.results.bindings){
+		var prk = result.results.bindings[i].n.value;
+        var bdind = parseFloat(result.results.bindings[i].value.value);
+		var count = result.results.bindings[i].count.value;
+		var area_val = parseFloat(result.results.bindings[i].area.value);
+		if(user_option=='>'){
+			 if(bdind > user_input){
+		        dataPoints.push({label: prk, y: bdind,toolTipContent: prk+":"+bdind+"<br>Count of Species: " +count +"<br>Area: "+area_val+"sq.Km."});
+				park_area.push(parseFloat(result.results.bindings[i].area.value));
+			 }
+		 }
+		 else if(user_option=='<'){
+			 if(bdind < user_input){
+		        dataPoints.push({label: prk, y: bdind,toolTipContent: prk+":"+bdind+"<br>Count of Species: " +count +"<br>Area: "+area_val+"sq.Km."});
+				park_area.push(parseFloat(result.results.bindings[i].area.value));
+			 }
+		 }
+		 else {dataPoints.push({label: prk, y: bdind, toolTipContent: prk+":"+bdind+"<br>Count of Species: " +count +"<br>Area: "+area_val+"sq.Km."});
+		 park_area.push(parseFloat(result.results.bindings[i].area.value));
+		       }
+		}
+		console.log(park_area);
+		var color_shades=[];
+		for (i in park_area){
+			if(park_area[i]<=1000){
+				color_shades.push("#99ff99");
+			}
+			else if(park_area[i]>1000 && park_area[i]<=10000){
+				color_shades.push("#339933");
+			}
+			else{
+				color_shades.push("#003300");
+			}
+		}
+		console.log(color_shades);
+		inter=dataPoints.length;
+		if(inter==49){inter=5;}
+		else {inter=1;}
+		CanvasJS.addColorSet("area_shade",color_shades); 
+		var chart = new CanvasJS.Chart("chartContainer",{
+        title:{
+            text:"Biodiversity Index of the Parks",
+			fontSize:15
+        },
+		height: 500,
+		colorSet:"area_shade",
+        animationEnabled: true,
+		axisX: {
+		title: "Parks",
+		labelAngle: -60,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		tickThickness: 1,
+		titleFontColor: "black",
+		interval: inter
+		},
+		axisY: {
+        title: "Biodiversity Index",
+		gridThickness: 1,
+		fontSize:20,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		tickThickness: 1,
+		titleFontColor: "black"
+		},
+		theme: "theme1",
+		data: [{
+        type: "column",
+		fillOpacity: 0.90,
+        dataPoints : dataPoints
+        }]
+		});
+		chart.render();
+			
+			//var info = result.results.bindings;
+        }, 
+        error: function(xhr, textStatus, errorThrown){ 
+            alert("Unable to fetch Server data");             	 	
+        }
+    }); 
+}
+
+function getDeforestationChart() {
+	     document.getElementsByClassName('low')[0].style.backgroundColor = "#ffb3b3";
+		 document.getElementsByClassName('medium')[0].style.backgroundColor = "#e60000";
+		 document.getElementsByClassName('high')[0].style.backgroundColor = "#800000";
+	     user_input=$("#Index").val();
+		 user_input=parseFloat(user_input);
+		 user_option=$("#DI").val();
+	var query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://purl.org/goodrelations/v1#>\nSelect ?p ?value ?n ?count ?area \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?p prop:hasProperty ?parks.\n?p foaf:name ?n.\n?parks uco:propertyName "DeforestationIndex".\n?parks gr:hasValue ?value.\n?p pr:SIO_000955.rdf ?sp.\n?sp pr:SIO_000794.rdf ?count.\n?p mea:Area ?b_area.\n?b_area gr:hasValue ?area.}\n}order By ?n';
+
+    var dataPoints = [];
+	var park_area=[];
+	$.ajax({
+        url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query),
+        method: "GET",
+        dataType: "jsonp",
+        async:false,
+        success: function(result){
+		//console.log(result);
+		for (var i in result.results.bindings){
+		var prk = result.results.bindings[i].n.value;
+        var defInd = parseFloat(result.results.bindings[i].value.value);
+		var count = parseFloat(result.results.bindings[i].count.value);
+		var area_val = parseFloat(result.results.bindings[i].area.value);
+		if(user_option=='>'){
+			 if(defInd >= user_input){
+		        dataPoints.push({label: prk, y: defInd,toolTipContent: prk+":"+defInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+				park_area.push(area_val);
+			 }
+		 }
+		 else if(user_option=='<'){
+			 if(defInd <= user_input){
+		        dataPoints.push({label: prk, y: defInd,toolTipContent: prk+":"+defInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+				park_area.push(area_val);
+			 }
+		 }
+		 else {dataPoints.push({label: prk, y:defInd, toolTipContent: prk+":"+defInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+		 park_area.push(area_val);}
+		}
+		
+		var color_shades=[];
+		for (i in park_area){
+			if(park_area[i]<=1000){
+				color_shades.push("#ffb3b3");
+			}
+			else if(park_area[i]>1000 && park_area[i]<=10000){
+				color_shades.push("#e60000");
+			}
+			else{
+				color_shades.push("#800000");
+			}
+		}
+		CanvasJS.addColorSet("area_shade",color_shades); 
+		inter=dataPoints.length;
+		if(inter==49){inter=5;}
+		else {inter=1;}
+		var chart = new CanvasJS.Chart("chartContainer",{
+        title:{
+            text:"Deforestation Index of the Parks",
+			fontSize:15
+        },
+        animationEnabled: true,
+		height: 500,
+		colorSet : "area_shade",
+        animationEnabled: true,
+		axisX: {
+		title: "Parks",
+		labelAngle: -60,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		titleFontColor: "black",
+		tickThickness: 1,
+		interval: inter
+		},
+		axisY: {
+        title: "Deforestation Index",
+		gridThickness: 1,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		tickThickness: 1,
+		titleFontColor: "black"
+		},
+		theme: "theme1",
+		data: [{
+        type: "column",
+		fillOpacity: 0.90,
+		dataPoints : dataPoints
+        }]
+		});
+		chart.render();
+        }, 
+        error: function(xhr, textStatus, errorThrown){ 
+            alert("Unable to fetch Server data");             	 	
+        }
+    }); 
+}
+
+function getSocialChart() {
+	     document.getElementsByClassName('low')[0].style.backgroundColor = "#99bbff";
+		 document.getElementsByClassName('medium')[0].style.backgroundColor = "#0000ff";
+		 document.getElementsByClassName('high')[0].style.backgroundColor = "#000066";
+	     user_input=$("#Index").val();
+		 user_input=parseFloat(user_input);
+		 user_option=$("#DI").val();
+	var query = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX wo: <http://purl.org/ontology/wo/>\nPREFIX bio: <http://purl.org/NET/biol/ns#>\nPREFIX txn: <http://lod.taxonconcept.org/ontology/txn.owl#>\nPREFIX foaf: <http://xmlns.com/foaf/0.1/>\nPREFIX dp:<http://dbpedia.org/page/>\nPREFIX wd: <http://purl.org/ontology/wo/>\nPREFIX loc: <http://www.ontotext.com/proton/protontop#>\nPREFIX mea:<http://def.seegrid.csiro.au/isotc211/iso19103/2005/basic#>\nPREFIX prop:<https://purl.oclc.org/NET/ssnx/ssn#>\nPREFIX park:<http://course.geoinfo2016.org/G2/>\nPREFIX pr:<http://semanticscience.org/resource/>\nPREFIX uco:<http://ontologies.makolab.com/uco/ns.html#>\nPREFIX gr:<http://purl.org/goodrelations/v1#>\nSelect ?value ?n ?count ?area \nWhere\n{\ngraph <http://course.geoinfo2016.org/G2>{\n?p foaf:name ?n.\n?p loc:locatedIn ?dep.\n?dep prop:hasProperty ?b.\n?b uco:propertyName "SocialIndex".\n?b gr:hasValue ?value.\n?p pr:SIO_000955.rdf ?sp.\n?sp pr:SIO_000794.rdf ?count.\n?p mea:Area ?b_area.\n?b_area gr:hasValue ?area.\n}\n}order By ?n';
+
+    var dataPoints = [];
+	var park_area = [];
+	$.ajax({
+        url: 'http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql?output=JSON&query=' + encodeURIComponent(query),
+        method: "GET",
+        dataType: "jsonp",
+        async:false,
+        success: function(result){
+		//console.log(result);
+		for (var i in result.results.bindings){
+		var prk = result.results.bindings[i].n.value;
+        var socInd = parseFloat(result.results.bindings[i].value.value);
+		var count = parseFloat(result.results.bindings[i].count.value);
+		var area_val = parseFloat(result.results.bindings[i].area.value);
+		if(user_option=='>'){
+			 if(socInd >= user_input){
+		        dataPoints.push({label: prk, y: socInd,toolTipContent: prk+":"+socInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+				park_area.push(area_val);
+			 }
+		 }
+		 else if(user_option=='<'){
+			 if(socInd <= user_input){
+		        dataPoints.push({label: prk, y: socInd,toolTipContent: prk+":"+socInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+				park_area.push(area_val);
+			 }
+		 }
+		 else {dataPoints.push({label: prk, y:socInd, toolTipContent: prk+":"+socInd+"<br>Count of Species: " +count +"<br>Area: "+area_val+" sq.Km."});
+		  park_area.push(area_val);}
+		}
+		
+		var color_shades=[];
+		for (i in park_area){
+			if(park_area[i]<=1000){
+				color_shades.push("#99bbff");
+			}
+			else if(park_area[i]>1000 && park_area[i]<=10000){
+				color_shades.push("#0000ff");
+			}
+			else{
+				color_shades.push("#000066");
+			}
+		}
+		CanvasJS.addColorSet("area_shade",color_shades); 
+		inter=dataPoints.length;
+		if(inter==49){inter=5;}
+		else {inter=1;}
+		var chart = new CanvasJS.Chart("chartContainer",{
+        title:{
+            text:"Social Index of the Parks",
+			fontSize:15
+        },
+        animationEnabled: true,
+		colorSet: "area_shade",
+		height: 500,
+        animationEnabled: true,
+		axisX: {
+		title: "Parks",
+		labelAngle: -60,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		titleFontColor: "black",
+		tickThickness: 1,
+		interval: inter
+		},
+		axisY: {
+        title: "Social Index",
+		gridThickness: 1,
+		labelFontSize: 13,
+		labelFontColor: "black",
+		titleFontSize: 18,
+		tickThickness: 1,
+		titleFontColor: "black"
+		},
+		theme: "theme1",
+		data: [{
+        type: "column",
+		fillOpacity: 0.90,
+		dataPoints : dataPoints
+        }]
+		});
+		chart.render();
+        }, 
+        error: function(xhr, textStatus, errorThrown){ 
+            alert("Unable to fetch Server data");             	 	
+        }
+    }); 
+}
--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 # GIIS-MapApp
 
+## About
 > Study project in winter term 2016/17 at ifgi for coursework GI in Society
 
+The goal of this study project was to built a Web application for Conservation of Natuaral Reserves in Columbia. It helps in answering following questions
+
+1. Helping Tourists in finding a location which is safe to visit and has particular specie type.
+2. Help researchers to find habitat of a particular specie and in which park they can be found.
+3. Help researchers to understand the spatial distribution of specie and recommend conservation strategies.
+4. Help policy makers to set conservation priorities based on information like deforestation index.
+
+## Summary
+Visualizing the conservation status of natural reserves and its endangered species for Columbia. Open Data was collected from multi-institutinal websites and converted into Open Linked Data (RDF triples). A web application was then developed using Leaflet and external plug-ins.
+
+## Technologies
+
+* [Leaflet](http://leafletjs.com/)
+
+* [Linked Data](http://linkeddata.org/)
+
+* [CanvasJS](http://canvasjs.com/)
+
+* JQuery & Bootstrap
+
+List of other dependencies can be found in package.json file.
+
+## Architecture
+
+![Architecture](img/architecture.png)
+
+## Functionalities
+
+### Map View
+
+![Map](img/Map.png)
+
+#### Navigation
+
+![Nav-Bar](img/NavBar.png)
+
+#### Search-Park
+
+![Search-Park](img/SearchPark.png)
+
+### Statistics
+![Charts](img/Charts.png)
+### About
+![About](img/About.png)
